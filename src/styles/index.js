56d66c01e369f2c409be66bfa4844bc161848700
Abhisheknanda1344463 <abhisheknanda94@gmail.com>
2021-02-01 13:33:57 +0530

    Minor Changes

--- src/styles/index.js
+++ src/styles/index.js
@@ -3,12 +3,20 @@ export default (editor, config) => {
   const csm = config.customStyleManager;
 
   sm.getSectors().reset(csm && csm.length ? csm : [{
-    name: 'Style Options',
-    open: false,
-    buildProps: ['display', 'width',  'height', 'color'],
-    properties: [{
-      name: 'Color',
-      property: 'color'
-    }],
+      name: 'Style Options',
+      open: false,
+      buildProps: ['width',  'height', 'color'],
+      properties: [{
+        property: 'display',
+        name: 'Visibility',
+        type: 'radio',
+        properties: [
+          {value: 'Visible', className: ''},
+          {value: 'Hidden', className: ''}
+        ],
+      },{
+        name: 'Color',
+        property: 'color'
+      }]
   }]);
 }
